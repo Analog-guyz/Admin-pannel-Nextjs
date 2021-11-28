3268762d7bf87d7ecfc2420aef2d69484b949ebf
Analog_guy <pooyan.cwz@gmail.com>
2021-11-28 18:56:07 +0330

    adding searchbar to team

--- pages/TeamMembersPage.js
+++ pages/TeamMembersPage.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from "../styles/TeamMembersPage.module.scss"
 import TeamMembers_card from '../components/TeamMembers_card'
 import {TeamMembersData} from "../DummyData/TeamData"
+import SearchIcon from '@material-ui/icons/Search';
 const TeamMembersPage = () => {
-    const img_src="https://images.unsplash.com/photo-1631640687399-067a45d2dd9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-    const location ="Tehran"
+    const [searchInput, setSearchIput] = useState("")
+
+
     return (
         <>
-        
+        {console.log(searchInput)}
         <div className={styles.TeamMembersPage_container}>
             <div className={styles.header}>
                 <p>Team Members</p>
+                <div className={styles.search_wrapper}>
+                    <input type="text" placeholder="Search name here" className={styles.Search_input} onChange={(e)=> {setSearchIput(e.target.value)}}/>
+                    <button type="submit" ><SearchIcon fontSize="small"/></button>
                 </div>
-            
-            {TeamMembersData.map((Data)=>{
+            </div>
+
+            {TeamMembersData.filter((val)=>{
+                if (searchInput == ""){
+                    return val
+                }
+                else if (val.name.toLowerCase().includes(searchInput.toLowerCase())){
+                    return val
+                }
+            }).map((Data)=>{
                 return (
                     <div key={Data.id}>
                         <TeamMembers_card name={Data.name} role={Data.role} img={Data.picture} location={Data.location}/>
